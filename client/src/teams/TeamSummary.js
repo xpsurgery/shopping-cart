61e7a514ce037aa4bdbcf6f27a98948521d64ed1
Kevin Rutherford <kevin@rutherford-software.com>
2017-05-14 20:45:52 +0100

    More styling on the setup page

--- client/src/teams/TeamSummary.js
+++ client/src/teams/TeamSummary.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default ({ name, colour, cash_balance }) =>
-  <div className='team-summary'>
+  <div className='team-summary' style={{'border-color': colour}}>
     <div className='team-colour' style={{ backgroundColor:colour }}> &nbsp; </div>
     <div className='team-name'> {name} </div>
     <div className='team-balance'> {cash_balance} </div>
